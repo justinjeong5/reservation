3968db75bccfb130384c14cd17f37aac13d86283
justinjeong <justin.jeong5@gmail.com>
2020-10-11 23:12:05 +0900

    restyle: 변수 수정 및 논리 개선

--- public/js/main.js
+++ public/js/main.js
@@ -31,36 +31,24 @@ const showProducts = (payload) => {
 }
 
 const getProducts = (payload) => {
-    const alertContents = () => {
+    const httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        console.log('XMLHTTP error');
+        return false;
+    }
+    httpRequest.onreadystatechange = () => {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if (httpRequest.status === 200) {
                 const list = JSON.parse(httpRequest.responseText);
                 const base = document.querySelector('#product__count');
-                const count = document.createTextNode(list.totalCount + '개');
-                if (base === null) {
-                    console.log("again getProducts")
-                    getProducts(payload);
-                    return;
-                    // base가 웹에 호출되는 시점이 매번 바뀌는 것을 관찰함
-                    // 경우에따라 base가 null인데 접근하려는 시도가 있음.
-                    // 이때 null을 참조하여 error가 발생하여, 이를 막는 코드임
-                    // 비동기 처리를 이용하여 해결하는 방법이 있으줄로 알지만, 알아내지못하여 임시방편을 씀
-                }
                 if (!base.hasChildNodes()) {
+                    const count = document.createTextNode(list.totalCount + '개');
                     base.appendChild(count);
                 }
                 showProducts(list);
-            } else {
-                console.log('request error');
             }
         }
-    }
-    var httpRequest = new XMLHttpRequest();
-    if (!httpRequest) {
-        console.log('js.getProducts XMLHTTP error');
-        return;
-    }
-    httpRequest.onreadystatechange = alertContents;
+    };
     if (payload.category === 0) {
         httpRequest.open('GET', 'api/products?start=' + productStartIndex);
     } else {
@@ -95,28 +83,19 @@ const showPromotions = (list) => {
 }
 
 const getPromotions = () => {
-    const alertContents = () => {
+    const httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        console.log('XMLHTTP error');
+        return false;
+    }
+    httpRequest.onreadystatechange = () => {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if (httpRequest.status === 200) {
                 const promotionList = JSON.parse(httpRequest.responseText);
-                const target = document.querySelector('#promotion__list');
-                if (target === null) {
-                    console.log("again getPromotions")
-                    getPromotions();
-                    return;
-                }
                 showPromotions(promotionList.items);
-            } else {
-                console.log('request error');
             }
         }
-    }
-    var httpRequest = new XMLHttpRequest();
-
-    if (!httpRequest) {
-        console.log('XMLHTTP error');
-    }
-    httpRequest.onreadystatechange = alertContents;
+    };
     httpRequest.open('GET', 'api/promotions');
     httpRequest.send();
 }
@@ -134,50 +113,50 @@ const showCategories = (list) => {
     const bindTemplate = Handlebars.compile(template);
     let innerHTMLCategories = '';
     for (const item of lists) {
-        console.log(item);
         innerHTMLCategories += bindTemplate(item);
     }
     target.innerHTML = innerHTMLCategories;
 }
 
 const getCategories = () => {
-    const alertContents = () => {
+    const httpRequest = new XMLHttpRequest();
+    if (!httpRequest) {
+        console.log('XMLHTTP error');
+        return false;
+    }
+    httpRequest.onreadystatechange = () => {
         if (httpRequest.readyState === XMLHttpRequest.DONE) {
             if (httpRequest.status === 200) {
                 const categoriesList = JSON.parse(httpRequest.responseText);
-                const target = document.querySelector("#category__list");
-                if (target === null) {
-                    console.log("again getcategories")
-                    getCategories();
-                    return;
-                }
                 showCategories(categoriesList.items)
-            } else {
-                console.log('request error');
             }
         }
-    }
-    var httpRequest = new XMLHttpRequest();
-
-    if (!httpRequest) {
-        console.log('XMLHTTP error');
-    }
-    httpRequest.onreadystatechange = alertContents;
+    };
     httpRequest.open('GET', 'api/categories');
     httpRequest.send();
 }
 
-/**
-<li class="item">
-    <a href="detail.html?id=${id}" class="item_book">
-        <div class="item_preview">
-            <img alt="${description}" class="img_thumb" src="http://211.249.62.123/productImages/${id}?type=th">
-            <span class="img_border"></span>
-        </div>
-        <div class="event_txt">
-            <h4 class="event_txt_tit"> <span>${description}</span> <small class="sm">${placeName}</small> </h4>
-            <p class="event_txt_dsc">${content}</p>
-        </div>
-    </a>
-</li>
- */
+const insertHTML = () => {
+    const elementList = document.querySelectorAll('#insert-html');
+    for (const element of elementList) {
+        const file = element.getAttribute('insert-html');
+        if (!file) continue
+        const httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = function() {
+            if (this.readyState === XMLHttpRequest.DONE) {
+                if (this.status === 200) { element.innerHTML = this.responseText; }
+                if (this.status === 404) { element.innerHTML = 'Page not found.'; }
+                element.removeAttribute('insert-html');
+                element.removeAttribute('id');
+            }
+        }
+        httpRequest.open('GET', file, true);
+        httpRequest.send();
+    }
+    getProducts({
+        category: 0
+    });
+    getPromotions();
+    getCategories();
+    showNextPromotion();
+};
